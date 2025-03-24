038a32978453613a6241e49c55c0de6ee8d85b9b
yasiru_k <yasiru_k@epiclanka.net>
2025-03-24 10:31:28 +0530

    Problem 2 done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -12,8 +14,22 @@ public class Main {
         System.out.println("===========================================================");
 
 
+        System.out.println("================= Problem 2 ================================");
+
+        List<String> list1 = Arrays.asList("a", "b", "c");
+        List<Integer> list2 = Arrays.asList(1, 2, 3);
+
+        List<Object> result = mergeAlternately(list1, list2);
+        System.out.println(result);
+
+
+
+
+
     }
 
+
+    //============================ Problem 1 =========================================
     public static int sumForLoop(List<Integer> numbers) {
         int sum = 0;
         for (int num : numbers) {
@@ -37,4 +53,27 @@ public class Main {
         }
         return numbers.get(index) + sumRecursion(numbers, index + 1);
     }
+
+
+
+    //====================================== Problem 2 =======================================
+
+
+        public static List<Object> mergeAlternately(List<?> list1, List<?> list2) {
+            List<Object> result = new ArrayList<>();
+
+            int i = 0, j = 0;
+            while (i < list1.size() || j < list2.size()) {
+                if (i < list1.size()) result.add(list1.get(i++));
+                if (j < list2.size()) result.add(list2.get(j++));
+            }
+
+            return result;
+        }
+
+
+
+    //====================================== Problem 3 =======================================
+
+
 }
